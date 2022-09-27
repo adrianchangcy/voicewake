b42383f5838af833bd18ba1f23c52cd0cf7fd3a4
adrianchangcy <adrianchangcy@gmail.com>
2022-09-27 17:05:52 +0800

    woo nav menu button

--- frontend/src/components/NavBar.vue
+++ frontend/src/components/NavBar.vue
@@ -1,34 +1,49 @@
 <template>
-    <nav class="overflow-y-scroll scroll-smooth fixed top-0 w-full max-h-full py-5     text-left text-3xl leading-none      backdrop-blur bg-theme-light/40">
-        <ul class="w-auto      grid grid-flow-row grid-cols-1 auto-rows-max gap-2 place-content-around">
-            <li class="flex flex-nowrap">
-                <a href="/" class="flex-1 mx-5 p-5 items-center      grid grid-cols-4      border-l-2 border-t-2 border-white/50 hover:border-white/70      hover:bg-theme-light/20      shadow-md hover:shadow-lg transition duration-100 ease-in-out hover:translate-x-1 hover:translate-y-1 rounded-lg hover:scale-105">
-                    <i class="col-span-1    fas fa-wave-square"></i>
+    <nav class="overflow-y-auto scroll-smooth fixed top-0 w-full max-h-full py-4    grid gap-2     text-left text-3xl leading-none      backdrop-blur bg-theme-light/40">
+        <ul class="w-auto mx-5      text-center      grid grid-flow-col grid-cols-4 gap-2">
+            <li class="col-span-2 flex flex-nowrap">
+                <a href="/" class="flex-1 py-4      border-l-2 border-t-2 border-white/50 hover:border-white/70      hover:bg-theme-light/20      shadow-md hover:shadow-lg transition duration-100 ease-in-out hover:translate-x-1 hover:translate-y-1 rounded-lg hover:scale-y-105">
+                    <i class="align-middle fas fa-wave-square"></i>
                 </a>
             </li>
+            <li class="col-span-1">
+            </li>
+            <li class="col-span-1 flex flex-nowrap items-end">
+                <button class="flex-1 py-4      border-l-2 border-t-2 border-white/50 hover:border-white/70      hover:bg-theme-light/20      shadow-md hover:shadow-lg transition duration-100 ease-in-out hover:translate-x-1 hover:translate-y-1 rounded-lg hover:scale-y-105"
+                    @click="toggle_full_nav"
+                >
+                    <div class="menu-btn    grid grid-rows-3 grid-flow-row" ref="menu_btn_ref">
+                        <div class="menu-btn__burger-top        row-span-1"></div>
+                        <div class="menu-btn__burger-middle     row-span-1"></div>
+                        <div class="menu-btn__burger-bottom     row-span-1"></div>
+                    </div>
+                </button>
+            </li>
+        </ul>
+        <ul v-if="is_full_nav" class="w-auto      grid grid-flow-row grid-cols-1 auto-rows-max gap-2">
             <li class="flex flex-nowrap">
-                <a href="/" class="flex-1 mx-5 p-5 items-center       grid grid-cols-4      border-l-2 border-t-2 border-white/50 hover:border-white/70      hover:bg-theme-light/20       shadow-md hover:shadow-lg transition duration-100 ease-in-out hover:translate-x-1 hover:translate-y-1 rounded-lg hover:scale-105">
+                <a href="/" class="flex-1 mx-5 p-4 items-center       grid grid-cols-4      border-l-2 border-t-2 border-white/50 hover:border-white/70      hover:bg-theme-light/20       shadow-md hover:shadow-lg transition duration-100 ease-in-out hover:translate-x-1 hover:translate-y-1 rounded-lg hover:scale-y-105">
                     <i class="col-span-1    fas fa-microphone-lines"></i>
                     <span class="col-span-2">Talk</span>
                 </a>
             </li>
             <li class="flex flex-nowrap">
-                <a href="/" class="flex-1 mx-5 p-5 items-center      grid grid-cols-4      border-l-2 border-t-2 border-white/50 hover:border-white/70      hover:bg-theme-light/20       shadow-md hover:shadow-lg transition duration-100 ease-in-out hover:translate-x-1 hover:translate-y-1 rounded-lg hover:scale-105">
+                <a href="/" class="flex-1 mx-5 p-4 items-center      grid grid-cols-4      border-l-2 border-t-2 border-white/50 hover:border-white/70      hover:bg-theme-light/20       shadow-md hover:shadow-lg transition duration-100 ease-in-out hover:translate-x-1 hover:translate-y-1 rounded-lg hover:scale-y-105">
                     <i class="col-span-1    fas fa-calendar-plus"></i>
                     <span class="col-span-2">Request</span>
                 </a>
             </li>
             <!-- <li class="flex flex-nowrap">
-                <a href="/" class="flex-1 mx-5 p-5 items-center      grid grid-cols-4      border-l-2 border-t-2 border-white/50 hover:border-white/70      hover:bg-theme-light/20       shadow-md hover:shadow-lg transition duration-100 ease-in-out hover:translate-x-1 hover:translate-y-1 rounded-lg hover:scale-105">
+                <a href="/" class="flex-1 mx-5 p-4 items-center      grid grid-cols-4      border-l-2 border-t-2 border-white/50 hover:border-white/70      hover:bg-theme-light/20       shadow-md hover:shadow-lg transition duration-100 ease-in-out hover:translate-x-1 hover:translate-y-1 rounded-lg hover:scale-y-105">
                     <i class="col-span-1    fa-solid fa-user"></i>
                     <span class="col-span-2">Sign In</span>
                 </a>
             </li> -->
 
             <!--when signed in-->
-            <Menu as="li" class="grid row-auto grid-flow-row" v-slot="{ open }">
-                <div class="row-span-1 flex flex-nowrap z-10">
-                    <MenuButton class="flex-1 mx-5 p-5 items-center     text-left     grid grid-cols-4      border-l-2 border-t-2 border-white/50 hover:border-white/70      hover:bg-theme-light/20      shadow-md hover:shadow-lg transition duration-100 ease-in-out hover:translate-x-1 hover:translate-y-1 rounded-lg hover:scale-105">
+            <Menu as="li" class="grid row-auto grid-flow-row gap-2" v-slot="{ open }">
+                <div class="row-span-1 flex flex-nowrap">
+                    <MenuButton class="flex-1 mx-5 p-4 items-center     text-left     grid grid-cols-4      border-l-2 border-t-2 border-white/50 hover:border-white/70      hover:bg-theme-light/20      shadow-md hover:shadow-lg transition duration-100 ease-in-out hover:translate-x-1 hover:translate-y-1 rounded-lg hover:scale-y-105">
                         <i class="col-span-1    fa-solid fa-user"></i>
                         <span class="col-span-2">Account</span>
                         <!--put transition here, and make it the same as the dropdown-->
@@ -41,7 +56,7 @@
                     </MenuButton>
                 </div>
                 <div :class='{
-                        "row-span-1 bg-theme-light/40 shadow-sm flex flex-nowrap z-0 text-left leading-none  w-auto mx-5 p-5 mt-2": open,
+                        "row-span-1 bg-theme-light/40 shadow-sm flex flex-nowrap text-left leading-none  w-auto mx-5 p-4": open,
                     }'
                 >
                     <MenuItems class="flex-1 gap-2 divide-y">
@@ -55,7 +70,7 @@
                             class="grid grid-cols-4"
                             >
                                 <i class="col-span-1    fas fa-microphone-lines"></i>
-                                <span class="col-span-2">Past Talks</span>
+                                <span class="col-span-2 text-clip">Past Talks</span>
                             </a>
                         </MenuItem>
                         <MenuItem v-slot="{ active }" class="items-center p-2">
@@ -92,7 +107,6 @@
                             }"
                             href="/"
                             class="grid grid-cols-4"
-                            @hook:mounted="dodo"
                             >
                                 <span class="1"></span>
                                 <span class="col-span-2">can scroll? 2</span>
@@ -101,8 +115,6 @@
                     </MenuItems>
                 </div>
             </Menu>
-       
-
         </ul>
     </nav>
 </template>
@@ -112,9 +124,66 @@
 <script setup>
   
     import { Menu, MenuButton, MenuItems, MenuItem } from '@headlessui/vue';
-
 </script>
 
 
+<script>
 
-        
+    export default{
+        data(){
+            return {
+                'is_full_nav': false,
+            };
+        },
+        methods: {
+            toggle_full_nav(){
+                if(!this.is_full_nav){
+                    this.$refs.menu_btn_ref.classList.add('open-full');
+                }else{
+                    this.$refs.menu_btn_ref.classList.remove('open-full');
+                }
+                this.is_full_nav = !this.is_full_nav;
+            }
+        }
+    }
+</script>
+
+<style>
+    .menu-btn__burger-middle {
+    width: 50px;
+    height: 6px;
+    background: #fff;
+    border-radius: 5px;
+    transition: all .5s ease-in-out;
+    }
+    .menu-btn__burger-top,
+    .menu-btn__burger-bottom {
+    content: '';
+    position: absolute;
+    width: 50px;
+    height: 6px;
+    background: #fff;
+    border-radius: 5px;
+    transition: all .5s ease-in-out;
+    }
+    .menu-btn__burger-top {
+    transform: translateY(-16px);
+    }
+    .menu-btn__burger-bottom {
+    transform: translateY(16px);
+    }
+    /* ANIMATION */
+    .menu-btn.open-full .menu-btn__burger-middle {
+    transform: translateX(-50px);
+    background: transparent;
+    box-shadow: none;
+    }
+    .menu-btn.open-full .menu-btn__burger-top {
+    transform: rotate(45deg);
+    }
+    .menu-btn.open-full .menu-btn__burger-bottom {
+    transform: rotate(-45deg);
+    }
+</style>
+
+      
