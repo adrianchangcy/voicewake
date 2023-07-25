cf272bc3e6ca40e694fe1e920738845dfde60d69
adrianchangcy <adrianchangcy@gmail.com>
2023-07-25 11:19:43 +0800

    created VEventTools to combine likedislike and share features, as well as settled VPlayback is_loading=true design

--- frontend/src/components/skeleton/VEventCardSkeleton.vue
+++ frontend/src/components/skeleton/VEventCardSkeleton.vue
@@ -1,15 +1,16 @@
 <template>
     <div class="flex flex-col gap-2">
         <!--user-->
-        <div class="h-4 flex flex-row gap-2">
+        <div class="h-5 flex flex-row gap-2">
             <div class="w-4 h-full rounded-full skeleton"></div>
             <div class="w-[25%] h-full rounded-lg skeleton"></div>
         </div>
         <!--card-->
         <div class="w-full h-20 rounded-lg skeleton"></div>
-        <!--like/dislike-->
-        <div class="w-full h-10 grid grid-cols-8">
-            <div class="col-span-6 lg:col-span-4 h-full rounded-full skeleton"></div>
+        <!--VEventTools-->
+        <div class="w-full h-10 grid grid-cols-8 gap-1">
+            <div class="col-span-5 lg:col-span-4 h-full rounded-full skeleton"></div>
+            <div class="col-span-3 lg:col-span-2 h-full rounded-full skeleton"></div>
         </div>
     </div>
 </template>
