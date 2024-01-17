0e8dda7cb5844b764ade32229c0cba701df459ff
adrianchangcy <adrianchangcy@gmail.com>
2024-01-17 20:19:08 +0800

    removed migrations, fresh start

--- voicewake/settings.py
+++ voicewake/settings.py
@@ -179,11 +179,11 @@ WSGI_APPLICATION = 'voicewake.wsgi.application'
 DATABASES = {
     'default': {  
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'voicewake',
-        'USER': 'postgres',
-        'PASSWORD': 'localhost',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'NAME': os.environ['DB_NAME'],
+        'USER': os.environ['DB_USER'],
+        'PASSWORD': os.environ['DB_PASSWORD'],
+        'HOST': os.environ['DB_HOST'],
+        'PORT': os.environ['DB_PORT'],
     }
 }
 
