d609ad7b445fc8f7dd792896cab8efda46b63b81
adrianchangcy <adrianchangcy@gmail.com>
2024-09-15 01:16:08 +0800

    ensured prod deployment to AWS works

---
+++ docker-push-ecr-prod.sh
@@ -0,0 +1,27 @@
+#this script builds vw-stage images and deploys to ECR
+
+#this script does not deliver frontend and static files to S3
+#to do so:
+#run dev frontend Docker container, then: vite build --emptyOutDir -- --env=prod
+#run dev Django container, then: python manage.py collectstatic --settings=voicewake.settings.prod
+
+#at VSCode, open new Git Bash terminal
+#then type: "sh thisfile.sh"
+
+#create session
+aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 981060373951.dkr.ecr.us-east-1.amazonaws.com
+
+#build all services docker-compose-stage.yaml
+docker-compose --env-file ./env/prod.env --file docker-compose-prod.yaml up -d --no-deps --force-recreate --no-start --build
+
+#tag freshly built images with ECR tags
+docker tag vw_prod-django_celery_beat:latest 981060373951.dkr.ecr.us-east-1.amazonaws.com/vw_prod-django_celery_beat:latest
+docker tag vw_prod-django_celery_worker:latest 981060373951.dkr.ecr.us-east-1.amazonaws.com/vw_prod-django_celery_worker:latest
+docker tag vw_prod-django_gunicorn:latest 981060373951.dkr.ecr.us-east-1.amazonaws.com/vw_prod-django_gunicorn:latest
+docker tag vw_prod-nginx:latest 981060373951.dkr.ecr.us-east-1.amazonaws.com/vw_prod-nginx:latest
+
+#push to AWS ECR
+docker push 981060373951.dkr.ecr.us-east-1.amazonaws.com/vw_prod-django_celery_beat:latest
+docker push 981060373951.dkr.ecr.us-east-1.amazonaws.com/vw_prod-django_celery_worker:latest
+docker push 981060373951.dkr.ecr.us-east-1.amazonaws.com/vw_prod-django_gunicorn:latest
+docker push 981060373951.dkr.ecr.us-east-1.amazonaws.com/vw_prod-nginx:latest
--- docker-push-ecr-stage.sh
+++ docker-push-ecr-stage.sh
@@ -1,5 +1,10 @@
 #this script builds vw-stage images and deploys to ECR
 
+#this script does not deliver frontend and static files to S3
+#to do so:
+#run dev frontend Docker container, then: vite build --emptyOutDir -- --env=stage
+#run dev Django container, then: python manage.py collectstatic --settings=voicewake.settings.stage
+
 #at VSCode, open new Git Bash terminal
 #then type: "sh thisfile.sh"
 
@@ -7,16 +12,16 @@
 aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 981060373951.dkr.ecr.us-east-1.amazonaws.com
 
 #build all services docker-compose-stage.yaml
-docker-compose --env-file ./env/.env --file docker-compose-stage.yaml up -d --no-deps --force-recreate --no-start --build
+docker-compose --env-file ./env/stage.env --file docker-compose-stage.yaml up -d --no-deps --force-recreate --no-start --build
 
 #tag freshly built images with ECR tags
-docker tag vw_stage-django_celery_beat:latest 981060373951.dkr.ecr.us-east-1.amazonaws.com/vw_stage_django_celery_beat:latest
-docker tag vw_stage-django_celery_worker:latest 981060373951.dkr.ecr.us-east-1.amazonaws.com/vw_stage_django_celery_worker:latest
-docker tag vw_stage-django_gunicorn:latest 981060373951.dkr.ecr.us-east-1.amazonaws.com/vw_stage_django_gunicorn:latest
-docker tag vw_stage-nginx:latest 981060373951.dkr.ecr.us-east-1.amazonaws.com/vw_stage_nginx:latest
+docker tag vw_stage-django_celery_beat:latest 981060373951.dkr.ecr.us-east-1.amazonaws.com/vw_stage-django_celery_beat:latest
+docker tag vw_stage-django_celery_worker:latest 981060373951.dkr.ecr.us-east-1.amazonaws.com/vw_stage-django_celery_worker:latest
+docker tag vw_stage-django_gunicorn:latest 981060373951.dkr.ecr.us-east-1.amazonaws.com/vw_stage-django_gunicorn:latest
+docker tag vw_stage-nginx:latest 981060373951.dkr.ecr.us-east-1.amazonaws.com/vw_stage-nginx:latest
 
 #push to AWS ECR
-docker push 981060373951.dkr.ecr.us-east-1.amazonaws.com/vw_stage_django_celery_beat:latest
-docker push 981060373951.dkr.ecr.us-east-1.amazonaws.com/vw_stage_django_celery_worker:latest
-docker push 981060373951.dkr.ecr.us-east-1.amazonaws.com/vw_stage_django_gunicorn:latest
-docker push 981060373951.dkr.ecr.us-east-1.amazonaws.com/vw_stage_nginx:latest
+docker push 981060373951.dkr.ecr.us-east-1.amazonaws.com/vw_stage-django_celery_beat:latest
+docker push 981060373951.dkr.ecr.us-east-1.amazonaws.com/vw_stage-django_celery_worker:latest
+docker push 981060373951.dkr.ecr.us-east-1.amazonaws.com/vw_stage-django_gunicorn:latest
+docker push 981060373951.dkr.ecr.us-east-1.amazonaws.com/vw_stage-nginx:latest
