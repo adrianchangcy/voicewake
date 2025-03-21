1de4d2e3a4e7ea128757f5d99487afc613d42eda
adrianchangcy <adrianchangcy@gmail.com>
2025-03-21 22:58:18 +0800

    polished up VUserCard and added copyright to nav

--- frontend/src/components/medium/VUserCard.vue
+++ frontend/src/components/medium/VUserCard.vue
@@ -1,15 +1,89 @@
 <template>
     <div>
-        <div class="flex flex-col gap-2 px-2 py-6 pt-4 border-4 border-theme-black">
+        <div class="flex flex-col p-4 border-4 border-theme-black">
 
             <div class="flex flex-row items-center">
-                <FontAwesomeIcon icon="fas fa-user" class="text-2xl pl-2 pr-4"/>
-                <span class="text-2xl font-medium break-all">{{ username }}</span>
+                <!--username-->
+                <div class="w-full flex flex-row items-center gap-2">
+                    <FontAwesomeIcon icon="fas fa-user" class="text-2xl"/>
+                    <span class="text-2xl font-medium break-all">{{ username }}</span>
+                </div>
+                <!--opener for more options-->
+                <div
+                    ref="open_close_user_card_options_menu_button"
+                    class="items-end pl-2"
+                >
+                    <VActionBorder
+                        v-if="canUseUserActions"
+                        :class="[
+                            is_user_card_options_menu_open ? 'border-2 border-theme-black dark:border-dark-theme-white-2' : 'border-theme-gray-form-field dark:border-dark-theme-gray-form-field shade-border-when-hover',
+                            'w-10 flex flex-row focus-visible:-outline-offset-2'
+                        ]"
+                        @click="toggleUserCardOptionsMenu"
+                        prop-element-size="s"
+                        prop-font-size="s"
+                        prop-element="button"
+                        :prop-is-enabled="true"
+                        :prop-is-icon-only="true"
+                        type="button"
+                    >
+                        <span class="mx-auto flex items-center text-center">
+                            <FontAwesomeIcon icon="fas fa-ellipsis-vertical" class="text-base"/>
+                            <span class="sr-only">More options</span>
+                        </span>
+                    </VActionBorder>
+                </div>
+            </div>
+
+            <!--more options menu-->
+            <div class="h-0 relative">
+                <!--arrow-->
+                <div
+                    v-show="is_user_card_options_menu_open"
+                    class="z-30 w-2 h-2 absolute top-1 right-4 m-auto bg-theme-light dark:bg-theme-dark border-l-2 border-t-2 border-theme-black dark:border-dark-theme-white-2 rotate-45"
+                ></div>
+                <!--more options-->
+                <!--for child buttons, use click.stop to avoid closing on click-->
+                <div
+                    v-show="is_user_card_options_menu_open"
+                    v-click-outside="{
+                        bool_status_variable_or_callback: 'is_user_card_options_menu_open',
+                        refs_to_exclude: ['open_close_user_card_options_menu_button']
+                    }"
+                    class="absolute z-20 top-2 right-0 w-[50%] rounded-lg p-4 border-2 border-theme-black dark:border-dark-theme-white-2 bg-theme-light dark:bg-theme-dark"
+                >
+                    <!--block-->
+                    <VActionText
+                        ref="user_card_options_menu_option_0"
+                        @click.stop="handleBlock()"
+                        prop-element-size="s"
+                        prop-font-size="s"
+                        prop-element="button"
+                        :prop-is-enabled="!is_block_processing"
+                        :prop-is-icon-only="is_block_processing"
+                        type="button"
+                        class="w-full"
+                    >
+                        <div v-show="is_block_processing" class="w-full h-full flex items-center">
+                            <div class="mx-auto">
+                                <VLoading prop-element-size="s"/>
+                            </div>
+                        </div>
+                        <div v-show="!is_block_processing" class="w-full h-full flex items-center">
+                            <div class="mx-auto">
+                                <FontAwesomeIcon v-show="!is_blocked" icon="fas fa-ban" class="text-base"/>
+                                <FontAwesomeIcon v-show="is_blocked" icon="fas fa-check" class="text-base"/>
+                                <span v-show="!is_blocked" class="pl-2">Block</span>
+                                <span v-show="is_blocked" class="pl-2">Blocked</span>
+                            </div>
+                        </div>
+                    </VActionText>
+                </div>
             </div>
 
             <div
                 v-if="propHasDefaultActions"
-                class="flex flex-row gap-1"
+                class="pt-2 grid grid-cols-2 gap-1"
             >
                 <VActionBorder
                     @click="copyUserURL()"
@@ -17,43 +91,22 @@
                     prop-font-size="s"
                     prop-element="button"
                     type="button"
-                    class="w-[6rem]"
+                    class="col-span-1"
                 >
-                    <TransitionFade>
-                        <span v-if="has_shared === false" class="mx-auto flex items-center text-center">
-                            <FontAwesomeIcon icon="fas fa-share" class="text-base"/>
-                            <span class="pl-1">Share</span>
-                        </span>
-                        <span v-else class="mx-auto flex items-center text-center">
-                            <FontAwesomeIcon icon="fas fa-check" class="text-base"/>
-                            <span class="pl-1">Copied</span>
-                        </span>
-                    </TransitionFade>
-                </VActionBorder>
-
-                <VActionBorder
-                    v-if="canUseUserActions"
-                    @click="handleBlock()"
-                    prop-element-size="s"
-                    prop-font-size="s"
-                    prop-element="button"
-                    :prop-is-enabled="!is_block_processing"
-                    :prop-is-icon-only="is_block_processing"
-                    type="button"
-                    class="w-[7rem]"
-                >
-                    <div
-                        v-if="is_block_processing"
-                        class="mx-auto"
-                    >
-                        <VLoading prop-element-size="s"/>
-                    </div>
-                    <span v-else class="mx-auto flex items-center text-center">
-                        <FontAwesomeIcon v-show="!is_blocked" icon="fas fa-ban" class="text-base"/>
-                        <FontAwesomeIcon v-show="is_blocked" icon="fas fa-check" class="text-base"/>
-                        <span v-show="!is_blocked" class="pl-1">Block</span>
-                        <span v-show="is_blocked" class="pl-1">Blocked</span>
-                    </span>
+                    <TransitionGroupFade>
+                        <div v-show="!has_shared" class="w-full h-full flex items-center">
+                            <div class="mx-auto">
+                                <FontAwesomeIcon icon="fas fa-share" class="text-base"/>
+                                <span class="pl-2">Share</span>
+                            </div>
+                        </div>
+                        <div v-show="has_shared" class="w-full h-full flex items-center">
+                            <div class="mx-auto">
+                                <FontAwesomeIcon icon="fas fa-check" class="text-base"/>
+                                <span class="pl-2">Copied</span>
+                            </div>
+                        </div>
+                    </TransitionGroupFade>
                 </VActionBorder>
 
                 <VActionBorder
@@ -65,25 +118,22 @@
                     :prop-is-enabled="!is_follow_processing"
                     :prop-is-icon-only="is_follow_processing"
                     type="button"
-                    class="w-[7rem]"
+                    class="col-span-1"
                 >
-                    <div
-                        v-if="is_follow_processing"
-                        class="mx-auto"
-                    >
-                        <VLoading prop-element-size="s"/>
+                    <div v-show="is_follow_processing" class="w-full h-full flex items-center">
+                        <div class="mx-auto">
+                            <VLoading prop-element-size="s"/>
+                        </div>
                     </div>
-                    <span v-else class="mx-auto flex items-center text-center">
-                        <FontAwesomeIcon v-show="!is_following" icon="far fa-star" class="text-base"/>
-                        <FontAwesomeIcon v-show="is_following" icon="fas fa-check" class="text-base"/>
-                        <span v-show="!is_following" class="pl-1">Follow</span>
-                        <span v-show="is_following" class="pl-1">Following</span>
-                    </span>
+                    <div v-show="!is_follow_processing" class="w-full h-full flex items-center">
+                        <div class="mx-auto">
+                            <FontAwesomeIcon v-show="!is_following" icon="fas fa-star" class="text-base"/>
+                            <FontAwesomeIcon v-show="is_following" icon="fas fa-check" class="text-base"/>
+                            <span v-show="!is_following" class="pl-2">Follow</span>
+                            <span v-show="is_following" class="pl-2">Following</span>
+                        </div>
+                    </div>
                 </VActionBorder>
-            </div>
-
-            <div v-else class="text-xl font-medium pl-2">
-                <slot></slot>
             </div>
         </div>
     </div>
@@ -94,7 +144,8 @@
     // import { Menu, MenuButton, MenuItems, MenuItem } from '@headlessui/vue';
     import VLoading from '../small/VLoading.vue';
     import VActionBorder from '../small/VActionBorder.vue';
-    import TransitionFade from '@/transitions/TransitionFade.vue';
+    import VActionText from '../small/VActionText.vue';
+    import TransitionGroupFade from '@/transitions/TransitionGroupFade.vue';
 
     import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
     import { library } from '@fortawesome/fontawesome-svg-core';
@@ -103,9 +154,10 @@
     import { faShare } from '@fortawesome/free-solid-svg-icons/faShare';
     import { faCheck } from '@fortawesome/free-solid-svg-icons/faCheck';
     import { faBan } from '@fortawesome/free-solid-svg-icons/faBan';
-    import { faStar } from '@fortawesome/free-regular-svg-icons/faStar';
+    import { faStar } from '@fortawesome/free-solid-svg-icons/faStar';
+    import { faEllipsisVertical } from '@fortawesome/free-solid-svg-icons/faEllipsisVertical';
 
-    library.add(faCircle, faUser, faShare, faCheck, faBan, faStar);
+    library.add(faCircle, faUser, faShare, faCheck, faBan, faStar, faEllipsisVertical);
 </script>
 
 
@@ -127,6 +179,8 @@
                 username: '',
                 has_shared: false,
                 has_shared_timeout: null as number|null,
+
+                is_user_card_options_menu_open: false,
 
                 is_blocked: false,
                 is_block_processing: false,
@@ -218,6 +272,10 @@
                     this.has_shared_timeout !== null ? window.clearTimeout(this.has_shared_timeout) : null;
                     this.has_shared_timeout = null;
                 }, 2000);
+            },
+            toggleUserCardOptionsMenu(){
+
+                this.is_user_card_options_menu_open = !this.is_user_card_options_menu_open;
             },
         },
         beforeMount(){
