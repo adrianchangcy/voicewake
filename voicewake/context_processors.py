11b5bb3cfaabe3ae4183f8b0ce47753678b3f773
adrianchangcy <adrianchangcy@gmail.com>
2025-12-09 01:19:07 +0800

    some small insignificant change to declare variables at common.py to avoid having so muxh os.environ at other code

--- voicewake/context_processors.py
+++ voicewake/context_processors.py
@@ -6,7 +6,7 @@ def settings_values(request):
     if settings.DEBUG is True:
 
         return {
-            'STATIC_CACHE_BUST_VERSION': settings.STATIC_CACHE_BUST_VERSION,
+            'STATIC_CACHE_BUST_URL_APPEND': settings.STATIC_CACHE_BUST_URL_APPEND,
             'DEBUG': settings.DEBUG,
             'VITE_PORT': os.environ['VITE_PORT'],
             'HOST': request.get_host().split(":")[0],
@@ -15,6 +15,6 @@ def settings_values(request):
     else:
 
         return {
-            'STATIC_CACHE_BUST_VERSION': settings.STATIC_CACHE_BUST_VERSION,
+            'STATIC_CACHE_BUST_URL_APPEND': settings.STATIC_CACHE_BUST_URL_APPEND,
             'DEBUG': settings.DEBUG,
         }
